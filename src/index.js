6d1a602c0d425fe5c9e2140c20661e3a67eecc54
Mrtrieu69 <trieutam2808@gmail.com>
2021-12-28 23:41:42 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,37 +1,48 @@
-const path = require("path")
-const express = require("express")
-const handlebars = require("express-handlebars")
-const morgan = require("morgan")
-const app = express()
-const port = 3000
+const path = require('path');
+const express = require('express');
+const handlebars = require('express-handlebars');
+const morgan = require('morgan');
+const app = express();
+const port = 3000;
+
+const route = require('./routes');
+const db = require("./config/db")
+
+// connect to DB
+db.connect()
+
+
 
 // Sửa đổi đường dẫn mặc định "/" -> "src/public/"
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Gọi middleware để xử lý dữ liệu được submit từ form bằng method POST
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined"))
-
+// app.use(morgan("combined"))
 
 // Template engine
 // Sửa đổi đuôi file template mặc định "handlebars" -> "hbs"
-app.engine("hbs", handlebars({
-  extname: ".hbs"
-}))
-app.set("view engine", "hbs")
+app.engine(
+    'hbs',
+    handlebars({
+        extname: '.hbs',
+    }),
+);
+app.set('view engine', 'hbs');
 
 // Thay đổi đường dẫn mặc định vào file views
-app.set("views", path.join(__dirname, "resources/views"))
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
-
-
-app.get('/', (req, res) => {
-  res.render("home")
-})
-
-app.get('/news', (req, res) => {
-  res.render("news")
-})
+// Route init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`App listening at http://localhost:${port}`);
+});
